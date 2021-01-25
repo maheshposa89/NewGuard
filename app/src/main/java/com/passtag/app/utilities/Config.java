19c0e4c66766db2cb8d357ffff3ffa8be2d62b23
suresh <ksuresh.unique@gmail.com>
2021-01-25 12:51:40 +0530

    test commit

--- app/src/main/java/com/passtag/app/utilities/Config.java
+++ app/src/main/java/com/passtag/app/utilities/Config.java
@@ -15,18 +15,7 @@ public class Config {
     public static final String help = BASE_URL + "Api/help";
 }
 
-    /*public static final String BASE_URL = "http://live.passtag.co.in/";
-    public static final String login = BASE_URL + "Api/user_login";
-    public static final String todayslist = BASE_URL + "Api/todays_guest";
-    public static final String upcominglist = BASE_URL + "Api/upcoming_guest";
-    public static final String homelist = BASE_URL + "Api/home_list";
-    public static final String addguest = BASE_URL + "Api/add_guest";
-    public static final String guestcheck = BASE_URL + "Api/fetch_guest";
-    public static final String deleteguest = BASE_URL + "Api/cancel";
-    public static final String splashscreen = BASE_URL + "Api/splash";
-    public static final String refcodes = BASE_URL + "Api/fetch_user_details";
-    public static final String signup = BASE_URL + "Api/signup";
-    public static final String help = BASE_URL + "Api/help";*/
+
 /*  public static final String login="http://18.221.138.210/guestpass/api/user_login";
     public static final String todayslist="http://18.221.138.210/guestpass/api/todays_guest";
     public static final String upcominglist="http://18.221.138.210/guestpass/api/upcoming_guest";
